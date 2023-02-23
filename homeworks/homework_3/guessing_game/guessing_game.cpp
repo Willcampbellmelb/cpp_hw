c51fb6dc8553e157513ca56c35a098bcc26a395f
Willcampbellmelb <wacampbel@gmail.com>
2023-02-22 17:09:44 -0800

    adding guessing game

---
+++ homeworks/homework_3/guessing_game/guessing_game.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int GenerateRandomNum(int small, int large) {
+  // generate random number
+  std::random_device random_device;
+  std::mt19937 random_engine{random_device()};
+  std::uniform_int_distribution<int> distribution{small, large};
+  return distribution(random_engine);
+}
+void playGame() {
+  int smallest{};
+  int largest{};
+  int guess{};
+  int num_gueses{0};
+  bool win{false};
+
+  cout << "Welcome to the GUESSING GAME !\n I will generate a number and you "
+          "will guess it !\n ";
+  cout << "Please provide the smallest number:\n";
+  cin >> smallest;
+  // get largest
+  cout << "Please provide the largest number:\n";
+  cin >> largest;
+  // generate random number
+
+  int rand_num = GenerateRandomNum(smallest, largest);
+  cout << "Random number " << rand_num << endl;
+
+  while (!win) {
+    num_gueses++;
+    cout << "Please provide the next guess:";
+    cin >> guess;
+    if (guess < rand_num) {
+      cout << "Your number is too small. Try again!\n";
+    } else if (guess > rand_num) {
+      cout << "Your number is too big. Try again!\n";
+    } else {
+      // Winner winner chicken dinner
+      cout << "You've done it! You guessed the number " << rand_num << " in "
+           << num_gueses << " guesses!\n";
+      win = true;
+    }
+  }
+}
+
+int main() {
+  playGame();
+
+  return 0;
+}
