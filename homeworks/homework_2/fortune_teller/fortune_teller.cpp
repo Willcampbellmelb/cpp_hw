fc52d6273c12d67db11255bd1d39ad53f035653e
Willcampbellmelb <wacampbel@gmail.com>
2023-02-24 14:41:52 -0800

    "adding user input checking"

--- homeworks/homework_2/fortune_teller/fortune_teller.cpp
+++ homeworks/homework_2/fortune_teller/fortune_teller.cpp
@@ -10,15 +10,97 @@
  */
 // version one without objects
 // version two uses user objects
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <map>
+#include <random>
+#include <sstream>
 #include <string>
 #include <vector>
+
+using std::cerr;
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+std::map<string, string> noun = {{"spring", "STL guru"},
+                                 {"summer", "C++ expert"},
+                                 {"autumn", "coding beast"},
+                                 {"winter", "software design hero"}};
 
+const std::vector<string> ending{"eats UB for breakfast",
+                                 "finds errors quicker than the compiler",
+                                 "is not afraid of C++ error messages"};
+// validate user input
+string GetLine() {
+  string result;
+  getline(cin, result);
+  return result;
+}
+
+int GetInteger() {
+  while (true) {
+    // get line from user and put into a string stream
+    std::stringstream ss;
+    int result{};
+    ss << GetLine();
+    // check that user put in an int
+    if (ss >> result) {
+      char remaining;
+      // check that is all they put in
+      if (ss >> remaining) {
+        cout << "Unexpected character:" << remaining << endl;
+      } else {
+        return result;
+      }
+    } else {
+      cout << "Please enter an integer!\n";
+    }
+    cout << "Retry: ";
+  }
+}
+bool allAlpha(std::string str) {
+  auto it = std::find_if_not(str.begin(), str.end(),
+                             [](char const &c) { return isalpha(c); });
+  return it == str.end(); // if we reach end of string - all values alpha
+}
+string GetWord() {
+  while (true) {
+    std::stringstream converter;
+    converter << GetLine();
+    string input{};
+    if (converter >> input) {
+      cout << "user input" << input << endl;
+      char remainder{};
+      if (converter >> remainder) {
+        cout << "Unexpected input character " << remainder << endl;
+      } else {
+        // check word comprised of all aphabetic chars
+        if (allAlpha(input)) {
+          return input;
+        } else {
+          cout << "user input not alpha numeric characters: " << input << endl;
+        }
+      }
+    } else {
+      cout << "please input a single word!\n";
+    }
+    cout << "Retry: ";
+  }
+}
+string GetSeason() {
+  while (true) {
+    string season = GetWord();
+    // check that word is in season map
+    if (noun.count(season)) {
+      return season;
+    } else {
+      cout << "user input missmatch:" << season << " is not a valid input\n";
+    }
+    cout << "Retry (pick from 'spring', 'summer', 'autumn', 'winter'):";
+  }
+}
 using std::string_literals::operator""s;
 /**
  * @brief Read the two adjectives from the user inputs in and store them in an
@@ -28,8 +110,7 @@ length of
 // the provided <NAME> to get an index that you can use to get an adjective from
 your array of adjectives
 */
-
-std::string lookup_ending(string b) {
+string lookup_ending(string b) {
   string ending = "The end";
   return ending;
 }
@@ -43,42 +124,24 @@ void printFortune(std::string name, std ::string season, std::string adj,
   // <NAME>, the <ADJECTIVE> <NOUN> that <ENDING>
   cout << name + ", the "s + adj + " " + noun + " that "s + ending << endl;
 }
-std::map<string, string> noun = {{"spring", "STL guru"},
-                                 {"summer", "C++ expert"},
-                                 {"autumn", "coding beast"},
-                                 {"winter", "software design hero"}};
 
-const std::vector<string> ending{"eats UB for breakfast",
-                                 "finds errors quicker than the compiler",
-                                 "is not afraid of C++ error messages"};
 int main() {
-  // Print a welcome statement:
-  cout << "Welcome to the fortune teller program!\n";
-  // Ask the user for their name:
-  std::string name;
-  std::string season;
-  std::string adjective_01;
-  std::string adjective_02;
   std::vector<string> adj{};
-  cout << "Please enter your name:\n";
-  cin >> name;
-
-  // Enter Ask the user when they were born:
+  cout << "Welcome to the fortune teller program!\n";
+  cout << "Please enter your first name:\n";
+  string name = GetWord();
   cout << "Please enter the time of year when you were born:\n"
           "(pick from 'spring', 'summer', 'autumn', 'winter')\n";
-  cin >> season; // need to type check this
-  // Ask for adjectives:
+  string season = GetSeason();
   cout << "Please enter an adjective:\n";
-  cin >> adjective_01; // type check this
-  adj.emplace_back(adjective_01);
+  string adjective_01 = GetWord();
   cout << "Please enter another adjective:\n";
-  cin >> adjective_02; // type check this
+  string adjective_02 = GetWord();
+  adj.emplace_back(adjective_01);
   adj.emplace_back(adjective_02);
   uint adj_index = name.size() % adj.size() - 1;
   adj_index = adj_index > adj.size() - 1 ? adj.size() - 1
                                          : adj_index; // check out of bounds
-  cout << adj_index << endl;
-
   uint end_index = name.size() % ending.size();
   adj_index = end_index > ending.size() - 1 ? ending.size() - 1
                                             : end_index; // check out of bounds
--- homeworks/homework_3/guessing_game/guessing_game.cpp
+++ homeworks/homework_3/guessing_game/guessing_game.cpp
@@ -1,12 +1,74 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <random>
+#include <sstream>
 #include <string>
 #include <vector>
 
+using std::cerr;
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+
+// validate user input
+string GetLine() {
+  string result;
+  getline(cin, result);
+  return result;
+}
+
+int GetInteger() {
+  while (true) {
+    // get line from user and put into a string stream
+    std::stringstream ss;
+    int result{};
+    ss << GetLine();
+    // check that user put in an int
+    if (ss >> result) {
+      char remaining;
+      // check that is all they put in
+      if (ss >> remaining) {
+        cout << "Unexpected character:" << remaining << endl;
+      } else {
+        return result;
+      }
+    } else {
+      cout << "Please enter an integer!\n";
+    }
+    cout << "Retry: ";
+  }
+}
+bool allAlpha(std::string str) {
+  auto it = std::find_if_not(str.begin(), str.end(),
+                             [](char const &c) { return isalpha(c); });
+  return it == str.end(); // if we reach end of string - all values alpha
+}
+string GetWord() {
+  while (true) {
+    std::stringstream converter;
+    converter << GetLine();
+    string input{};
+    if (converter >> input) {
+      cout << "user input" << input << endl;
+      char remainder{};
+      if (converter >> remainder) {
+        cout << "Unexpected input character " << remainder << endl;
+      } else {
+        // check word comprised of all aphabetic chars
+        if (allAlpha(input)) {
+          return input;
+        } else {
+          cout << "user input not alpha numeric characters: " << input << endl;
+        }
+      }
+    } else {
+      cout << "please input a single word!\n";
+    }
+    cout << "Retry: ";
+  }
+}
 
 int GenerateRandomNum(int small, int large) {
   // generate random number
@@ -25,10 +87,10 @@ void playGame() {
   cout << "Welcome to the GUESSING GAME!\n"
        << "I will generate a number and you will guess it!\n"
        << "Please provide the smallest number:\n";
-  cin >> smallest;
+  smallest = GetInteger();
   // get largest
   cout << "Please provide the largest number:\n";
-  cin >> largest;
+  largest = GetInteger();
   // generate random number
 
   const int rand_num = GenerateRandomNum(smallest, largest);
@@ -36,12 +98,7 @@ void playGame() {
   while (!win) {
     num_gueses++;
     cout << "Please provide the next guess: ";
-    if (!(std::cin >> guess)) {
-      std::cerr << "Invalid input. Please enter a valid integer." << std::endl;
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-      continue;
-    }
+    guess = GetInteger();
     if (guess < rand_num) {
       cout << "Your number is too small. Try again!\n";
     } else if (guess > rand_num) {
